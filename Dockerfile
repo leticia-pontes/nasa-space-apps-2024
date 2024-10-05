8d8452dfb1dcbbb59dc3c1d58917d8ceb7cd802e
Leticia Alves de Pontes <50830736+leticia-hub@users.noreply.github.com>
2024-10-05 20:04:46 -0300

    Setting Dockerfile and entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM python:3.9-slim
+RUN apt-get update && apt-get install -y dos2unix && rm -rf /var/lib/apt/lists/*
 WORKDIR /app
 RUN pip install --no-cache-dir pandas dash dash-bootstrap-components
 COPY . .
+RUN dos2unix entrypoint.sh utils/download_file.py
 RUN chmod +x entrypoint.sh
 EXPOSE 8050
 ENTRYPOINT ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-echo "Baixando arquivo..."
-python download_file.py
+set -e
 
-if [ $? -ne 0 ]; then
-    echo "Erro ao baixar arquivo. Saindo..."
+echo "Diretório atual: $(pwd)"
+
+if [ -d "utils/" ]; then
+    cd utils/
+else
+    echo "O diretório utils/ não existe. Saindo..."
     exit 1
 fi
+
+if [ -f "download_file.py" ]; then
+    python download_file.py
+else
+    echo "O arquivo download_file.py não foi encontrado no diretório utils/. Saindo..."
+    exit 1
+fi
+
+echo "Arquivo baixado com sucesso."
+
+cd ..
 
 echo "Iniciando o servidor..."
 python app.py
